552ed5ec7eec54a504d03f29cfa1526f5288d30c
Hamza Ba'aqeel <h.m.b.2002@hotmail.com>
2024-05-18 23:35:10 +0300

    test2

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const userRoutes = require('./routes/userRoutes');
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.use('/api/users', userRoutes);
+
+// const PORT = process.env.PORT ;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
