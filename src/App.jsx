25edea2c8c1352646ad0879dc7c65b2fb89fb4bf
Hamza Ba'aqeel <h.m.b.2002@hotmail.com>
2024-05-18 23:59:57 +0300

    test44

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,61 @@
-
+import React, { useEffect, useState } from 'react';
 import Footer from './HomePage-Components/Footer';
 import Navbar from './HomePage-Components/Navbar';
 import Home from './pages/Home.jsx';
 import SearchP from './pages/SearchP.jsx';
 import SignUp from './pages/SignUp.jsx';
-import profile from './pages/profile.jsx';
-import log_in from './pages/LogIn.jsx';
-
-
+import Profile from './pages/profile.jsx';
+import LogIn from './pages/LogIn.jsx';
+// import NotFoundPage from './pages/NotFoundPage.jsx';
 
 const App = () => {
-  console.log(window.location.pathname)
-  let Page
-  switch(window.location.pathname){
-    case "/":
-      Page = SearchP
-      break
-    case "/Search":
-    Page = Home
-      break
-    case "/SignUp":
-    Page = SignUp
-      break
-    case "/Profile":
-      Page = profile
-      break
-    case "/add_book":
-      Page = add_book
-      break
-    case "/LogIn":
-      Page = log_in
-      break
-  }
-  if(Page === SignUp || Page === log_in){
-    return(<Page />)
-  }
-  else{
-  return (
+  const [currentPage, setCurrentPage] = useState(window.location.pathname);
 
+  useEffect(() => {
+    const handlePopState = () => {
+      setCurrentPage(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
+  let Page;
+  switch(currentPage) {
+    case "/":
+      Page = Home ;
+      break;
+    case "/Search":
+      Page = SearchP;
+      break;
+    case "/SignUp":
+      Page = SignUp;
+      break;
+    case "/Profile":
+      Page = Profile;
+      break;
+    case "/add_book":
+      Page = AddBook; // Make sure to define/import AddBook
+      break;
+    case "/LogIn":
+      Page = LogIn;
+      break;
+    default:
+      Page = NotFoundPage; // Fallback for undefined routes
+  }
+
+  const isAuthPage = Page === SignUp || Page === LogIn;
+
+  return (
     <div className='App'>
-      <Navbar />
+      {!isAuthPage && <Navbar />}
       <Page />
-      <Footer />
+      {!isAuthPage && <Footer />}
     </div>
   );
-}
-
-}
+};
 
 export default App;
-
-
